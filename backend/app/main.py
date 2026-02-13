1db474a209cf486ffd0c343db171f51deda637d6
RohanPal1 <104245611+RohanPal1@users.noreply.github.com>
2026-02-13 20:36:55 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -59,7 +59,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        '*'
+        "*"
     ],
     allow_credentials=False,
     allow_methods=["*"],
